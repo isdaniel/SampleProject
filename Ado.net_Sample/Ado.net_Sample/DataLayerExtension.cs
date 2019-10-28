8ef44b5d8097fda4de4b1a68fa4bcb060ac4d892
Daniel <dog830228@gmail.com>
2019-10-28 21:34:03 +0800

    add ado.net sample.

---
+++ Ado.net_Sample/Ado.net_Sample/DataLayerExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace Ado.net_Sample
+{
+    public static class DataLayerExtension
+    {
+        public static IEnumerable<T> ExecuteReader<T>(this IDbCommand cmd,Func<IDataReader,T> func)
+        {
+            List<T> result = new List<T>();
+
+            using (var dr = cmd.ExecuteReader())
+            {
+                while (dr.Read())
+                {
+                    result.Add(func(dr));
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Ado.net_Sample/Ado.net_Sample/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ado.net_Sample
+{
+    class Program
+    {
+        private static string _conn = ConfigurationManager.ConnectionStrings["DbConn"].ConnectionString;
+
+        static void Main(string[] args)
+        {
+            string password = "test123";
+            string sql = @"SELECT UserId,
+	  UserName,
+	  [PassWord]
+FROM dbo.UserAccount
+WHERE password = @password;";
+
+            //建立SqlConnection物件
+            using (var conn = new SqlConnection(_conn))
+            {
+                //打開與資料庫的連接
+                conn.Open();
+
+                //建立Sql的命令類別
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = sql;
+                    //預設是 CommandType.Text
+                    cmd.CommandType = CommandType.Text;
+
+                    //如果有使用到參數 添加參數
+                    var para = new SqlParameter(
+                        "@password", SqlDbType.VarChar, 100)
+                    {
+                        Value = password
+                    };
+                    cmd.Parameters.Add(para);
+
+                    //從料庫中讀取資料
+                    using (var dr = cmd.ExecuteReader())
+                    {
+                        //判斷是否還有下一筆資料
+                        while (dr.Read())
+                        {
+                            Console.WriteLine($"{dr["UserId"]} {dr["UserName"]} {dr["PassWord"]}");
+                        }
+                    }
+                }
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
