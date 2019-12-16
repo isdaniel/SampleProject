3389e1be037713ad45cabf7dae52435eeb1584e9
Daniel.Shih <dog830228@gmail.com>
2019-12-16 17:15:08 +0800

    add sample.

--- HttpSample/HttpSample/Program.cs
+++ HttpSample/HttpSample/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data;
 using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
@@ -16,6 +17,13 @@ using RestSharp;
 
 namespace HttpSample
 {
+    public class PersonModel
+    {
+        public int UserID { get; set; }
+        public string UserName { get; set; }
+        public string PassWord{ get; set; }
+    }
+
     class Program
     {
         static string connStr = ConfigurationManager.ConnectionStrings["DbConn"].ToString();
@@ -75,27 +83,30 @@ namespace HttpSample
         static void Main(string[] args)
         {
             // 準備寫入的 data
-            int id = 1;
-            string url = "http://localhost:1209/User/UserInfo";
-            using (HttpClient httpClient= new HttpClient())
-            {
+            //int id = 1;
+            //string url = "http://localhost:1209/User/UserInfo";
+            //using (HttpClient httpClient= new HttpClient())
+            //{
                
-                // 將 data 轉為 json
-                string json = JsonConvert.SerializeObject(new {id});
-                // 將轉為 string 的 json 依編碼並指定 content type 存為 httpcontent
-                HttpContent contentPost = new StringContent(json, Encoding.UTF8, "application/json");
+            //    // 將 data 轉為 json
+            //    string json = JsonConvert.SerializeObject(new {id});
+            //    // 將轉為 string 的 json 依編碼並指定 content type 存為 httpcontent
+            //    HttpContent contentPost = new StringContent(json, Encoding.UTF8, "application/json");
 
-                HttpResponseMessage response = httpClient.PostAsync(new Uri(url),contentPost).Result;
-                response.EnsureSuccessStatusCode();
-                string responseBody = response.Content.ReadAsStringAsync().Result;
-                Console.WriteLine(responseBody);
-            }
+            //    HttpResponseMessage response = httpClient.PostAsync(new Uri(url),contentPost).Result;
+            //    response.EnsureSuccessStatusCode();
+            //    string responseBody = response.Content.ReadAsStringAsync().Result;
+            //    Console.WriteLine(responseBody);
+            //}
 
-
+        
             using (var conn = new SqlConnection(connStr))
             {
+                DynamicParameters parameters = new DynamicParameters();
+                parameters.Add("@UserName","Daniel",DbType.AnsiString,ParameterDirection.Input,100);
                 //打開連結
                 conn.Open();
+                var result = conn.Query<PersonModel>("[dbo].[Sample_Procedure]",parameters,commandType:CommandType.StoredProcedure);
             }
 
             Console.ReadKey();
